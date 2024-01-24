d84b52d61e293d78d767c3c7129f80217f106373
Ngozi Aguluka <melissacinta@gmail.com>
2024-01-24 18:57:09 +0100

    removed red bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="h-full bg-red-500 flex-col items-center justify-between p-24">
+    <main className="h-full flex-col items-center justify-between p-24">
       hello
     </main>
   );
