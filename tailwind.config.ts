04cadeceb4ea986274e59a05dd98bb928317b275
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-04-25 01:25:54 +0100

    color update + format

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,37 +1,41 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 // tailwind.config.js
-const { nextui } = require('@nextui-org/react');
+const { nextui } = require("@nextui-org/react");
 
 const config: Config = {
   content: [
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
         primary: {
-          10: '#FB3D3D', //red
-          20: '#444', //grey
-          30: '#BDBDBD', //light grey
-          40: '#302F2F', // sub header grey
+          10: "#FB3D3D", //red
+          20: "#444", //grey
+          30: "#BDBDBD", //light grey
+          40: "#302F2F", // sub header grey
         },
         secondary: {
-          10: '#545353', //grey color;
-          20: '#EFEFEF', //light grey bg on pill;
-          30: '#B8B8B8', //light grey text on pill;
-          40: '#C7C7C7', //dark gray border divide
+          10: "#545353", //grey color;
+          20: "#EFEFEF", //light grey bg on pill;
+          30: "#B8B8B8", //light grey text on pill;
+          40: "#C7C7C7", //dark gray border divide
+          50: "#2F2F2F", // Icon
+          60: "#010101", // Text Black
+          70: "#DFDDDD", // Text Black
+          80: "#059C1D", // Green BG
         },
       },
       fontFamily: {
-        aleo: ['Aleo', 'serif'],
+        aleo: ["Aleo", "serif"],
       },
     },
   },
-  darkMode: 'class',
+  darkMode: "class",
   plugins: [nextui()],
 };
 export default config;
