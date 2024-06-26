f77d1aa92c22d330e1720d125eddd4a4e16fd0c3
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-06-26 11:30:44 +0100

    auth page

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { DGLoginContainer } from "@/container/AuthPage";
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <>
+      <DGLoginContainer />
+    </>
+  );
+};
+
+export default page;
---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { DGSignupContainer } from "@/container/AuthPage";
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <>
+      <DGSignupContainer />
+    </>
+  );
+};
+
+export default page;
