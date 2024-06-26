4b773497b9760ecd290271952ef4774bbbf979c3
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-06-26 11:22:00 +0100

    chakra provider

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -9,8 +9,9 @@
 // app/providers.tsx
 "use client";
 
+import customTheme from "@/utils/customTheme";
 import { ChakraProvider } from "@chakra-ui/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider>{children}</ChakraProvider>;
+  return <ChakraProvider theme={customTheme}>{children}</ChakraProvider>;
 }
