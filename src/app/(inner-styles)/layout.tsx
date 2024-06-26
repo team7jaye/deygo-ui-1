dc3a486ee10f199eabd2c140cd84abefc0bc0258
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-06-26 11:22:48 +0100

    Deleted

--- src/app/(inner-styles)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Providers } from '../Providers';
-import '@/app/globals.css';
-export default function InnerLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <div>{children}</div>;
-}
