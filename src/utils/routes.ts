75dbe88f8190ace2e14c97a1ca1573cf42ef0734
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-06-26 15:54:31 +0100

    Routes

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,6 +1,13 @@
 export class Routes {
   // ###===> AUTH ROUTES
-  static login = "/login";
+  static login = "auth/login";
   static otp = "/otp";
   static signup = "/signup";
+
+  // ###===> MAIN ROUTES
+  static home = "/";
+  static businesses = (category: string, id: string | number) =>
+    `/${category}/${id}`;
+  static details = (id: string | number) =>
+    `${this.businesses}?BusinessId=${id}`;
 }
