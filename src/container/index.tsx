7231ce1ec4b7d10acd6e1a74f6e56f1ad7b2f52b
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-06-26 11:43:10 +0100

    containers

---
+++ src/container/index.tsx
@@ -0,0 +1,9 @@
+import DGBusinessPageContainer from "./BusinessesPage";
+import DGDetailsContainer from "./DetailsPage";
+import DGHomeCategoriesContainer from "./HomePage";
+
+export {
+  DGBusinessPageContainer,
+  DGDetailsContainer,
+  DGHomeCategoriesContainer,
+};
