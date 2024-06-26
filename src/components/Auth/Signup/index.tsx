39be6a95f89d1d2dfa514736f2c450b19beef10b
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-06-26 15:52:41 +0100

    update signup and added 2 step

--- src/components/Auth/Signup/index.tsx
+++ src/components/Auth/Signup/index.tsx
@@ -1,97 +1,208 @@
-'use client';
+"use client";
 
+import { Form, Formik } from "formik";
+
+import DGButton from "@/components/global/Button";
+import DGInput from "@/components/global/Input";
 import {
   Flex,
   Box,
   FormControl,
   FormLabel,
   Input,
-  InputGroup,
-  InputRightElement,
+  Checkbox,
   Stack,
   Button,
   Heading,
   Text,
   useColorModeValue,
-  Link,
-} from '@chakra-ui/react';
-import { useState } from 'react';
-import { FaEye, FaEyeSlash } from 'react-icons/fa';
+} from "@chakra-ui/react";
+import validationSchema from "@/utils/validationSchema";
+import Link from "next/link";
+import { Routes } from "@/utils/routes";
+import { useState } from "react";
+
+interface Values {
+  name: string;
+  email: string;
+  phoneNumber: string;
+  pin: string;
+}
 
 const SignupCard: React.FC = () => {
-  const [showPassword, setShowPassword] = useState(false);
+  const [step, setStep] = useState(1);
+  const initialValues: Values = {
+    name: "",
+    phoneNumber: "",
+    email: "",
+    pin: "",
+  };
+
+  const handleNext = (values: Values) => {
+    if (step === 1) {
+      setStep(2);
+    } else {
+      alert(JSON.stringify(values, null, 2));
+    }
+  };
+
+  const handlePrevious = () => {
+    setStep(step - 1);
+  };
 
   return (
-    <Flex
-      minH={'100vh'}
-      align={'center'}
-      justify={'center'}
-      bg={useColorModeValue('gray.50', 'gray.800')}
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema.signup}
+      onSubmit={(values, { setSubmitting }) => {
+        setSubmitting(true);
+        handleNext(values);
+        setSubmitting(false);
+      }}
     >
-      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-        <Stack align={'center'}>
-          <Heading fontSize={'4xl'} textAlign={'center'}>
-            Sign up
-          </Heading>
-          <Text fontSize={'lg'} color={'gray.600'}>
-            to enjoy all of our cool features ✌️
-          </Text>
-        </Stack>
-        <Box
-          rounded={'lg'}
-          bg={useColorModeValue('white', 'gray.700')}
-          boxShadow={'lg'}
-          p={8}
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        isValid,
+        /* and other goodies */
+      }) => (
+        <Flex
+          minH={"100vh"}
+          align={"center"}
+          justify={"center"}
+          bg={useColorModeValue("gray.50", "gray.800")}
         >
-          <Stack spacing={4}>
-            <FormControl id="name" isRequired>
-              <FormLabel>Name</FormLabel>
-              <Input type="text" />
-            </FormControl>
-            <FormControl id="phone" isRequired>
-              <FormLabel>Phone Number</FormLabel>
-              <Input type="tel" />
-            </FormControl>
-            <FormControl id="email" isRequired>
-              <FormLabel>Email Address</FormLabel>
-              <Input type="email" />
-            </FormControl>
-            <FormControl id="password" isRequired>
-              <FormLabel>Password</FormLabel>
-              <InputGroup>
-                <Input type={showPassword ? 'text' : 'password'} />
-                <InputRightElement h={'full'}>
-                  <Button
-                    variant={'ghost'}
-                    onClick={() => setShowPassword((showPassword) => !showPassword)}
-                  >
-                    {showPassword ? <FaEye /> : <FaEyeSlash />}
-                  </Button>
-                </InputRightElement>
-              </InputGroup>
-            </FormControl>
-            <Stack spacing={10} pt={2}>
-              <Button
-                loadingText="Submitting"
-                size="lg"
-                bg={'#FB3D3D'}
-                color={'white'}
-                _hover={{
-                  bg: 'blue.500',
-                }}
-              >
-                Create Account
-              </Button>
-            </Stack>
-            <Stack pt={6}>
-              <Text align={'center'}>
-                Already a user? <Link color={'blue.400'} href='/auth/login'>Login</Link>
+          <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+            <Stack align={"center"}>
+              <Heading fontSize={"4xl"}>Sign Up</Heading>
+              <Text fontSize={"lg"} color={"gray.600"}>
+                To access this feature, please create an account
               </Text>
             </Stack>
+            <Box
+              rounded={"lg"}
+              bg={useColorModeValue("white", "gray.700")}
+              boxShadow={"lg"}
+              p={8}
+            >
+              <Stack spacing={4}>
+                <Form>
+                  {step === 1 && (
+                    <>
+                      <DGInput
+                        name="name"
+                        type="text"
+                        label="Name"
+                        placeholder="Enter name"
+                        error={touched.name ? errors.name : null}
+                        onChange={handleChange}
+                        value={values.name}
+                      />
+                      <DGInput
+                        name="phoneNumber"
+                        type="tel"
+                        label="Phone Number"
+                        placeholder="Enter Phone Number"
+                        error={touched.phoneNumber ? errors.phoneNumber : null}
+                        onChange={handleChange}
+                        value={values.phoneNumber}
+                      />
+                      <DGInput
+                        name="email"
+                        type="email"
+                        label="Email"
+                        placeholder="Enter Email Address"
+                        error={touched.email ? errors.email : null}
+                        onChange={handleChange}
+                        value={values.email}
+                      />
+
+                      <Stack spacing={10}>
+                        <Stack
+                          direction={{ base: "column", sm: "row" }}
+                          align={"start"}
+                          justify={"space-between"}
+                        >
+                          {/* <DGInput  type="checkbox"/> */}
+                          <Checkbox>Remember me</Checkbox>
+                          <Link href={"/auth/login"}>
+                            <Text color={"blue.400"} cursor="pointer">
+                              Login instead
+                            </Text>
+                          </Link>
+                        </Stack>
+
+                        {/* <DGButton
+                          type="submit"
+                          bg={"primary.200"}
+                          color={"white"}
+                          _hover={{
+                            bg: "primary.500",
+                          }}
+                          isLoading={isSubmitting}
+                          // disabled
+                          disabled={!isValid || isSubmitting} // Disable if not valid or submitting
+                        >
+                          Create Account
+                        </DGButton> */}
+                      </Stack>
+                    </>
+                  )}
+                  {step === 2 && (
+                    <DGInput
+                      name="pin"
+                      type="password"
+                      label="Set Pin"
+                      placeholder="Enter a 4-digit pin"
+                      error={touched.pin ? errors.pin : null}
+                      onChange={handleChange}
+                      value={values.pin}
+                    />
+                  )}
+                  <Stack spacing={10}>
+                    <Stack
+                      direction={{ base: "column", sm: "row" }}
+                      align={"start"}
+                      justify={"space-between"}
+                    >
+                      {step === 2 && (
+                        <DGButton
+                          onClick={handlePrevious}
+                          bg={"gray.200"}
+                          color={"black"}
+                          _hover={{
+                            bg: "gray.400",
+                          }}
+                        >
+                          Back
+                        </DGButton>
+                      )}
+                    </Stack>
+                    <DGButton
+                      type="submit"
+                      bg={"primary.200"}
+                      color={"white"}
+                      _hover={{
+                        bg: "primary.500",
+                      }}
+                      isLoading={isSubmitting}
+                      disabled={!isValid || isSubmitting}
+                    >
+                      {step === 1 ? "Next" : "Create Account"}
+                    </DGButton>
+                  </Stack>
+                </Form>
+              </Stack>
+            </Box>
           </Stack>
-        </Box>
-      </Stack>
-    </Flex>
+        </Flex>
+      )}
+    </Formik>
   );
 };
 
