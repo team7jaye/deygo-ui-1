f5ff093555c88164cede3380cb3157690a53ecda
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-06-26 11:21:35 +0100

    Homepage

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { DGHomeCategoriesContainer } from "@/container";
+import { Box } from "@chakra-ui/react";
+import Image from "next/image";
+
+
+export default function Home() {
+  
+  return (
+    <main>
+      <DGHomeCategoriesContainer />
+    </main>
+  );
+}
