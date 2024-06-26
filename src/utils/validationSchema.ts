b934cba0a95dc3abe1d3942aa936fcbb28490928
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-06-26 15:54:45 +0100

    Validation Schema

--- src/utils/validationSchema.ts
+++ src/utils/validationSchema.ts
@@ -15,6 +15,13 @@ const validationSchema = {
   agentOTPVerify: yup.object().shape({
     otp: yup.string().min(6).max(6).required().label("OTP"),
   }),
+
+  signup: yup.object().shape({
+    name: yup.string().trim().required().label("Name"),
+    email: yup.string().trim().email().required().label("Email"),
+    phoneNumber: yup.string().required().trim().label("Phone Number"),
+  }),
+
 };
 
 export default validationSchema;
